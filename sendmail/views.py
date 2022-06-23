400d5b9965afd51fa4837112cc2f67e18993c558
Kota Nakasaka <web.knakasaka@gmail.com>
2022-06-23 15:44:32 +0900

    main

---
+++ sendmail/views.py
@@ -0,0 +1,25 @@
+from re import template
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.conf import settings
+from django.core.mail import BadHeaderError, EmailMessage, send_mail
+from django.http import HttpResponse
+import textwrap
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        subject = "【あしあと】配布したQRコードから貴社サイトにアクセスがありました"
+        message = request.get_full_path()
+        from_email = "web.knakasaka@gmail.com"
+        recipient_list = ["web.decube@gmail.com"]
+        try:
+            send_mail(subject, message, from_email, recipient_list)
+        except BadHeaderError:
+            return HttpResponse('無効なヘッダが検出されました。')
+        return render(request, 'sendmail/redirect.html')
+    template_name = 'index.html'
+
+class RedirectView(View):
+    template_name = 'redirect.html'
+    
